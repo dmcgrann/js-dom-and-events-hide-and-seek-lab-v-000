b24bd5d028a60e281bda2cd0479ebd69eef499fc
David McGrann <dpmcgrann@gmail.com>
2019-01-24 14:44:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('.ranked-list');
+    for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (i + n).toString();
+  }
+}
+
+function deepestChild() {
+  return document.querySelector( '#grand-node div div div div' );
+}
